b92c272d2b40db6ea38e67761243c0205fcf7d52
kevin46dsa <kevin0108dsa@gmail.com>
2023-05-26 14:53:10 -0400

    created component skeleton

---
+++ src/Components/ObjectDetector/ObjectDetector.jsx
@@ -0,0 +1,16 @@
+//add all imports
+
+function ObjectDetector() {
+
+// add logic to edit the image
+
+
+  return (
+    <div>
+      <h2>Test Object ObjectDetector</h2>
+      {/*input script*/}
+    </div>
+  );
+}
+
+export default ObjectDetector;
